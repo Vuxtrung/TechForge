cbe8b439a12fad780f89d86380977141e79b4579
NokoriPiiii <huy171205@gmail.com>
2026-08-12 08:04:05 +0900

    add more templates, login templates, footer, header fragments

--- src/main/java/com/swp391/techforge/controller/HomeController.java
+++ src/main/java/com/swp391/techforge/controller/HomeController.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/home")
+    public String redirectHome() {
+        return "redirect:/";
     }
 }
